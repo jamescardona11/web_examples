c04aaf6506fb61c8b4f6bb9bd9e37fb17abf7763
jamescardona11 <jjcardonao@gmail.com>
2021-07-10 17:45:41 -0500

    add pages in pageview

--- landing_page_scroll/lib/ui/pages/home_page.dart
+++ landing_page_scroll/lib/ui/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:landing_page_scroll/ui/views/about_view.dart';
+import 'package:landing_page_scroll/ui/views/contact_view.dart';
+import 'package:landing_page_scroll/ui/views/home_view.dart';
+import 'package:landing_page_scroll/ui/views/location_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -8,12 +12,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-        child: Center(
-          child: Text('Hello New Page'),
-        ),
+      body: Stack(
+        children: [
+          _HomeBody(),
+          Positioned(
+            right: 20,
+            top: 20,
+            child: Container(
+              width: 150,
+              height: 50,
+              color: Colors.black,
+            ),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  const _HomeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      children: [
+        HomeView(),
+        AboutView(),
+        ContactView(),
+        LocationView(),
+      ],
     );
   }
 }
