401645ef43548c56f31a440ab0119592823540ee
jamescardona11 <jjcardonao@gmail.com>
2021-07-11 17:46:32 -0500

    Add provider and change name or push when user set page in url

--- landing_page_scroll/lib/providers/page_provider.dart
+++ landing_page_scroll/lib/providers/page_provider.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart';
 
 class PageProvider extends ChangeNotifier {
-  final scrollController = PageController();
+  PageController scrollController = PageController();
+
+  List<String> _pages = ['home', 'about', 'contact', 'location'];
+  int _curentIndex = 0;
+
+  void createScrollController(String routeName) {
+    scrollController = PageController(initialPage: getPageIndex(routeName))
+      ..addListener(() {
+        final index = (scrollController.page ?? 0).round();
+
+        if (_curentIndex != index) {
+          window.history.pushState(null, '', '#/${_pages[index]}');
+          document.title = _pages[index];
+          _curentIndex = index;
+        }
+      });
+  }
+
+  int getPageIndex(String routeName) {
+    final index = _pages.indexOf(routeName);
+
+    if (index >= 0) {
+      return index;
+    }
+
+    return 0;
+  }
 
   void goTo(int index) {
+    // window.history.pushState(null, '', '#/${_pages[index]}');
+
     scrollController.animateToPage(
       index,
       duration: Duration(milliseconds: 200),
--- landing_page_scroll/lib/router/router_handlers.dart
+++ landing_page_scroll/lib/router/router_handlers.dart
@@ -1,10 +1,17 @@
 import 'package:fluro/fluro.dart';
+import 'package:landing_page_scroll/providers/page_provider.dart';
 import 'package:landing_page_scroll/ui/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 final homeHandler = Handler(
   handlerFunc: (context, params) {
     final page = params['page']!.first;
 
-    if (page != '/') return HomePage();
+    if (page != '/') {
+      final pageProvider = Provider.of<PageProvider>(context!, listen: false);
+      pageProvider.createScrollController(page);
+
+      return HomePage();
+    }
   },
 );
